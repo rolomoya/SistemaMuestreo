8d411fe71c697074f6feebc86f6145dfd94ce404
Rolo <rolomoya97@gmail.com>
2018-05-31 23:55:22 -0600

    Menu

---
+++ app/src/main/java/Actividad.java
@@ -0,0 +1,26 @@
+public class Actividad {
+
+    private String nombre;
+    private String descrip;
+
+    public Actividad(String nombre, String descrip) {
+        this.nombre = nombre;
+        this.descrip = descrip;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+}
