3c81d2d69b4272a5f5150dbf3779653542489992
Rolo <rolomoya97@gmail.com>
2018-06-03 16:04:49 -0600

    Menu

---
+++ app/src/main/java/proyectoreq/sistemamuestreo/AsignarAnalista.java
@@ -0,0 +1,13 @@
+package proyectoreq.sistemamuestreo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AsignarAnalista extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asignar_analista);
+    }
+}
