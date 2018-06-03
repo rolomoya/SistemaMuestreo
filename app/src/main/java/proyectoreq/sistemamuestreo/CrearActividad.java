3ac797c10a301b7d1a5e7e9b85c52eaa77838e69
audrarg <audrarg@hotmail.com>
2018-06-03 17:52:30 -0600

    Base de Datos

--- app/src/main/java/proyectoreq/sistemamuestreo/CrearActividad.java
+++ app/src/main/java/proyectoreq/sistemamuestreo/CrearActividad.java
@@ -11,7 +11,7 @@ public class CrearActividad extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_actividad);
-
+        ConexionSQLiteHelper conn = new ConexionSQLiteHelper(this,"bd_muestreos",null,1);
         Spinner spinnerTipos = findViewById(R.id.spinnerTiposActiv);
         ArrayAdapter<CharSequence> adaptTipos = ArrayAdapter.createFromResource(this,R.array.spinner_tipos,android.R.layout.simple_spinner_item);
 
