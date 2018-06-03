5dc4da0604082cc771f26047165c3ce50d25238a
Rolo <rolomoya97@gmail.com>
2018-06-03 14:54:27 -0600

    Menu

---
+++ app/src/main/java/App/Observacion.java
@@ -0,0 +1,6 @@
+package App;
+
+public class Observacion {
+    private int humedad;
+    private int temperatura;
+}
--- app/src/main/java/proyectoreq/sistemamuestreo/pantOperacion.java
+++ app/src/main/java/proyectoreq/sistemamuestreo/pantOperacion.java
@@ -2,6 +2,8 @@ package proyectoreq.sistemamuestreo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class pantOperacion extends Activity {
 
@@ -9,5 +11,12 @@ public class pantOperacion extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pant_operacion);
+    }
+
+    public void onClick(View view){
+        //aqui se llama la instrucción sql para guardar el nombre y descripcion de la operacion
+        String nomOp = ((EditText)findViewById(R.id.nombreOp)).getText().toString(); // String que se agarra del campo de nombre
+        String descOp = ((EditText)findViewById(R.id.descripOp)).getText().toString(); // String que se agarra del campo de descripcion
+
     }
 }
