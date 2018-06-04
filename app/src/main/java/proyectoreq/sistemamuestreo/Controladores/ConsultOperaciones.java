59f4e6ef08b1c3c1a8963c2245222810ee253e3d
Rolo <rolomoya97@gmail.com>
2018-06-03 20:08:42 -0600

    Cambios varios(pantalla reg, listview en crear operacion)

---
+++ app/src/main/java/proyectoreq/sistemamuestreo/Controladores/ConsultOperaciones.java
@@ -0,0 +1,15 @@
+package proyectoreq.sistemamuestreo.Controladores;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import proyectoreq.sistemamuestreo.R;
+
+public class ConsultOperaciones extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consult_operaciones);
+    }
+}
