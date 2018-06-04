678281d50395e50a174101471ec60a078d8ad9ab
audrarg <audrarg@hotmail.com>
2018-06-04 03:22:37 -0600

    Base de Datos : Insertar Tareas

--- app/src/main/java/proyectoreq/sistemamuestreo/Controladores/RegistrarTrabajador.java
+++ app/src/main/java/proyectoreq/sistemamuestreo/Controladores/RegistrarTrabajador.java
@@ -1,10 +1,16 @@
 package proyectoreq.sistemamuestreo.Controladores;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import proyectoreq.sistemamuestreo.R;
+import proyectoreq.sistemamuestreo.utilidades.Utilidades;
 
 public class RegistrarTrabajador extends Activity {
 
@@ -14,9 +20,33 @@ public class RegistrarTrabajador extends Activity {
         setContentView(R.layout.activity_registrar_trabajador);
     }
 
-    public void onClick(){
-        String nombreTrab = ((EditText) findViewById(R.id.campoNombreTrab)).getText().toString();
+    public void onClick(View view){
+        //aqui se llama la instrucción sql para guardar el nombre y descripcion de la operacion
+        registrar_Trabajador();
+    }
+
+    private void registrar_Trabajador() {
         String alias = ((EditText) findViewById(R.id.campoAlias)).getText().toString();
         String puesto = ((EditText) findViewById(R.id.campoPuesto)).getText().toString();
+
+        ConexionSQLiteHelper conn = new ConexionSQLiteHelper(this,"bd_muestreos",null,1);
+        SQLiteDatabase db = conn.getWritableDatabase();
+
+
+        try {
+            ContentValues values = new ContentValues();
+
+            values.put(Utilidades.Registro_AliasTrabajador,alias);
+            values.put(Utilidades.Registro_PuestoTrabajador,puesto);
+
+
+            Long idResult = db.insert(Utilidades.Tabla_Trabajadores,Utilidades.Registro_IdTrabajador,values);
+            Toast.makeText(getApplicationContext(), "Trabajador registrado correctamente", Toast.LENGTH_SHORT).show();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "No se pudo registrar el trabajador", Toast.LENGTH_SHORT).show();
+        }
+        db.close();
     }
 }
--- app/src/main/java/proyectoreq/sistemamuestreo/utilidades/Utilidades.java
+++ app/src/main/java/proyectoreq/sistemamuestreo/utilidades/Utilidades.java
@@ -41,14 +41,54 @@ public class Utilidades {
 
     public static final String Crear_Tabla_Trabajadores = "CREATE TABLE "+Tabla_Trabajadores+" ("+Registro_IdTrabajador+" Integer PRIMARY KEY AUTOINCREMENT, "+Registro_AliasTrabajador+" Text, "+Registro_PuestoTrabajador+" Text)";
 
+    //TABLA MUESTREOS
 
-    public static final String Crear_Tabla_Muestreos = "CREATE TABLE muestreos (idMuestreo Integer, descripcion Text, cantObservaciones Integer, idOperacion Integer, FechaInicio Text)";
+    public static final String Tabla_Muestreos = "muestreos";
+    public static final String Registro_IdMuestreo = "idMuestreo";
+    public static final String Registro_descripcionMuestreo = "descripcion";
+    public static final String Registro_cantObservaciones = "cantObservaciones";
+    public static final String Registro_FKidOPeracion = "idOperacion";
+    public static final String Registro_FechaMuestreo = "fechaInicio";
 
-    public static final String Crear_Tabla_ObservacionesXMuestreo = "CREATE TABLE observacionesXMuestreo (idOperacionXMuestreo Integer, idTarea Integer, idTrabajador Integer, idMuestreo Integer, fechaHora Text, temperatura Integer, humedad Integer)";
+    public static final String Crear_Tabla_Muestreos = "CREATE TABLE "+Tabla_Muestreos+" ("+Registro_IdMuestreo+" Integer PRIMARY KEY AUTOINCREMENT, "+Registro_descripcionMuestreo+" Text, "+Registro_cantObservaciones+" Integer, "+Registro_FKidOPeracion+" Integer, "+Registro_FechaMuestreo+" Text, FOREIGN KEY ("+Registro_FKidOPeracion+") REFERENCES "+Tabla_Operaciones+"("+Registro_IdOperacion+"))";
 
-    public static final String Crear_Tabla_TrabajadorXmuestreo = "CREATE TABLE trabajadoresXMuestreo (idTrabajadorXMuestreo Integer,  idTrabajador Integer, idMuestreo Integer)";
 
-    public static final String Crear_Tabla_UsuarioXMuestreo = "CREATE TABLE usuariosXMuestreo (idUsuarioXMuestreo Integer,  idUsuario Integer, idMuestreo Integer)";
+    //TABLA OBSERVACIONES X MUESTREOS
 
-    public static final String Crear_Tabla_TareasXOperacion = "CREATE TABLE tareasXoperacion (idTareaXoperacion Integer, idTarea Integer, idOperacion Integer)";
+    public static final String Tabla_observacionesXMuestreo = "observacionesXMuestreo";
+    public static final String Registro_IdObservacionesXMuestreo = "idObservacionesXMuestreo";
+    public static final String Registro_FKIdTarea = "idTarea";
+    public static final String Registro_FKIdTrabajador = "idTrabajador";
+    public static final String Registro_FKIdMuestreo = "idMuestreo";
+    public static final String Registro_FechaObservaciones = "fechaHora";
+    public static final String Registro_Temperatura = "temperatura";
+    public static final String Registro_Humedad = "humedad";
+
+    public static final String Crear_Tabla_ObservacionesXMuestreo = "CREATE TABLE "+Tabla_observacionesXMuestreo+" ("+Registro_IdObservacionesXMuestreo+
+                                " Integer PRIMARY KEY AUTOINCREMENT, "+Registro_FKIdTarea+" Integer, "+Registro_FKIdTrabajador+" Integer, "+Registro_FKIdMuestreo+
+                                " Integer, "+Registro_FechaObservaciones+" Text, "+Registro_Temperatura+" Integer, "+Registro_Humedad+" Integer, FOREIGN KEY ("+Registro_FKIdTarea+") REFERENCES "
+            +Tabla_Tareas+"(    "+Registro_IdTarea+"),FOREIGN KEY ("+Registro_FKIdTrabajador+") REFERENCES "+Tabla_Trabajadores+"("+Registro_IdTrabajador+"), FOREIGN KEY ("+Registro_FKIdMuestreo+") REFERENCES "+Tabla_Muestreos+"("+Registro_IdMuestreo+"))";
+
+    //TABLA TRABAJADOR X MUESTREO
+    public static final String Tabla_TrabajadorXMuestreo = "trabajadoresXMuestreo";
+    public static final String Registro_IdTrabajadorXMuestreo = "IdtrabajadoresXMuestreo";
+    public static final String Registro_FKIdTrabajador2 = "idTrabajador";
+    public static final String Registro_FKIdMuestreo2 = "idMuestreo";
+
+    public static final String Crear_Tabla_TrabajadorXmuestreo = "CREATE TABLE "+Tabla_TrabajadorXMuestreo+" ("+Registro_IdTrabajadorXMuestreo+" Integer PRIMARY KEY AUTOINCREMENT,  "+Registro_FKIdTrabajador2+" Integer, "+Registro_FKIdMuestreo2+" Integer,  FOREIGN KEY ("+Registro_FKIdMuestreo2+") REFERENCES "+Tabla_Muestreos+"("+Registro_IdMuestreo+"), FOREIGN KEY ("+Registro_FKIdTrabajador2+") REFERENCES "+Tabla_Trabajadores+"("+Registro_IdTrabajador+"))";
+
+    //TABLA USUARIO X MUESTREO
+
+    public static final String Tabla_UsuarioXMuestreo = "usuariosXMuestreo";
+    public static final String Registro_IdUsuarioXMuestreo = "IdusuariosXMuestreo";
+    public static final String Registro_FKIdUsuario = "idUsuario";
+    public static final String Registro_FKIdMuestreo3 = "idMuestreo";
+
+    public static final String Crear_Tabla_UsuarioXMuestreo = "CREATE TABLE "+Tabla_UsuarioXMuestreo+" ("+Registro_IdUsuarioXMuestreo+" Integer PRIMARY KEY AUTOINCREMENT,  "+Registro_FKIdUsuario+" Integer, "+Registro_FKIdMuestreo3+" Integer,  FOREIGN KEY ("+Registro_FKIdMuestreo3+") REFERENCES "+Tabla_Muestreos+"("+Registro_IdMuestreo+"), FOREIGN KEY ("+Registro_FKIdUsuario+") REFERENCES "+Tabla_Usuarios+"("+Registro_IdUsuario+") )";
+
+    public static final String Tabla_TareaXOperacion = "tareasXoperacion";
+    public static final String Registro_IdTareaXOperacion = "idTareaXoperacion";
+    public static final String Registro_FKIdTarea2 = "idTarea";
+    public static final String Registro_FKIdOperacion2 = "idOperacion";
+    public static final String Crear_Tabla_TareasXOperacion = "CREATE TABLE "+Tabla_TareaXOperacion+" ("+Registro_IdTareaXOperacion+" Integer PRIMARY KEY AUTOINCREMENT, "+Registro_FKIdTarea2+" Integer, "+Registro_FKIdOperacion2+" Integer, FOREIGN KEY ("+Registro_FKIdTarea2+") REFERENCES "+Tabla_Tareas+"("+Registro_IdTarea+"), FOREIGN KEY ("+Registro_FKIdOperacion2+") REFERENCES "+Tabla_Operaciones+"("+Registro_IdOperacion+") )";
 }
